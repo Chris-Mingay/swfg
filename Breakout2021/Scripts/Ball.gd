0f46e8da31b211c768516c84a915b042ed32584d
Chris Mingay <chris@chrismingay.co.uk>
2021-09-04 12:41:10 +0100

    Ball bouncy

--- Breakout2021/Scripts/Ball.gd
+++ Breakout2021/Scripts/Ball.gd
@@ -1,7 +1,20 @@
 extends RigidBody2D
 
+const SPEEDUP = 4
+const MAXSPEED = 300
+
 func _ready():
 	pass
 
 func _physics_process(delta):
 	var bodies = get_colliding_bodies()
+	
+	for body in bodies:
+		if body.is_in_group("Bricks"):
+			body.queue_free()
+			
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = get_position() - body.get_node("Anchor").get_global_position()
+			var velocity = direction.normalized() * min(speed+SPEEDUP*delta, MAXSPEED * delta)
+			set_linear_velocity(velocity)
--- Breakout2021/Scripts/Paddle.gd
+++ Breakout2021/Scripts/Paddle.gd
@@ -4,6 +4,6 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	self.global_position.x = get_viewport().get_mouse_position().x
-	pass
+	self.position.x = get_viewport().get_mouse_position().x
+
 
